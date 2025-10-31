641ae3cbeda2c93373965f31fb43c1fee298d9e4
sbejaoui <souheil.bejaoui@acsone.eu>
2025-10-31 19:09:46 +0100

    [18.0][ADD] rma_batch_sale_auto_detect
    This module extends the batch RMA process by adding automatic sale order
    linking at batch level. It reuses the matching logic provided by the module
    `rma_sale_auto_detect`, but allows it to be executed on all RMAs contained
    inside an `rma.batch`.
    
    A new batch state **Manual Treatment** is introduced.
    If at least one RMA in the batch fails to auto-match, the batch is moved to
    this state, so the user can review and fix the unmatched RMAs manually.
    
    refs: #473
    
    [IMP] rma_batch_sale_auto_detect: allow user to select product then so in batch view

--- rma_lot/tests/test_rma_lot.py
+++ rma_lot/tests/test_rma_lot.py
@@ -80,7 +80,7 @@ class TestRMALot(BaseCommon):
         return_wizard.create_rma = True
         return_wizard.rma_operation_id = self.operation
         self.assertEqual(len(return_wizard.product_return_moves), 2)
-        return_wizard.action_create_returns_all()
+        return_wizard.action_create_returns()
         self.assertEqual(self.picking.rma_count, 2)
         rmas = self.picking.move_ids.rma_ids
         rma_lot_1 = rmas.filtered(lambda r, lot=self.lot_1: r.lot_id == lot)
