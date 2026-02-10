bd4777b09a1501c4fd47e4ad847405aebf341fc9
Stéphane Mangin <stephane.mangin@webmel.com>
2026-02-10 11:36:42 +0100

    [18.0][ADD] rma_batch_reason: add new module to link batch and reason behaviors

---
+++ rma_batch_reason/models/rma.py
@@ -0,0 +1,25 @@
+# Copyright 2026 ACSONE SA/NV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import api, models
+
+
+class Rma(models.Model):
+    _inherit = "rma"
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        for vals in vals_list:
+            if vals.get("batch_id") and not vals.get("reason_id"):
+                batch = self.env["rma.batch"].browse(vals["batch_id"])
+                if batch.reason_id:
+                    vals["reason_id"] = batch.reason_id.id
+        return super().create(vals_list)
+
+    @api.depends("batch_id.reason_id")
+    def _compute_reason_id(self):
+        for rec in self:
+            if rec.state != "draft":
+                continue
+            if rec.batch_id.reason_id:
+                rec.reason_id = rec.batch_id.reason_id
---
+++ rma_batch_reason/tests/test_rma_batch_reason.py
@@ -0,0 +1,39 @@
+# Copyright 2026 ACSONE SA/NV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo.tests import tagged
+
+from .common import TestRmaBatchReasonCommon
+
+
+@tagged("-at_install", "post_install")
+class TestRmaBatchReason(TestRmaBatchReasonCommon):
+    def test_rma_creation_with_batch_reason(self):
+        """Test that RMAs created in a batch inherit the reason."""
+        batch = self._create_batch(reason=self.reason)
+        rma = self._create_rma(batch=batch)
+        self.assertEqual(rma.reason_id, self.reason)
+
+    def test_rma_form_batch_reason_update(self):
+        """Test that updating batch reason propagates to draft RMAs without reason."""
+        batch = self._create_batch()
+        rma = self._create_rma(batch=batch, reason=self.reason)
+        rma_without_reason = self._create_rma(batch=batch, reason=None)
+        batch.reason_id = self.other_reason
+        rma._compute_reason_id()
+        rma_without_reason._compute_reason_id()
+        self.assertEqual(rma.reason_id, batch.reason_id)
+        self.assertEqual(rma_without_reason.reason_id, self.other_reason)
+
+    def test_rma_form_batch_reason_update_when_not_draft(self):
+        """Test that updating batch reason does not propagate to non-draft RMAs."""
+        batch = self._create_batch(reason=self.reason)
+        rma = self._create_rma(batch=batch)
+        batch.action_ready()
+        batch.reason_id = self.other_reason
+        rma._compute_reason_id()
+        self.assertEqual(rma.reason_id, self.reason)
+        batch.action_confirm()
+        batch.reason_id = self.other_reason
+        rma._compute_reason_id()
+        self.assertEqual(rma.reason_id, self.reason)
